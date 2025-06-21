371cb99eac1acd16bb7ce13331fe63d40f7d9d64
Sebas Echazú <sebastianechazu@outlook.com>
2025-06-21 16:20:53 +0000

    feat: eliminar archivos de registro y optimizar configuración de Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -24,9 +24,6 @@ export const appConfig: ApplicationConfig = {
     provideStorage(() => getStorage()), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
-          }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
           })
   ]
 };
